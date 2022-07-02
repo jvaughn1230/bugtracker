5b4dcd84c7c4986dd6d11571b4dec7c1e35c1c64
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-02 16:10:49 -0700

    adding files for components

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Register() {   
   return (
-    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center'>
+    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center shadow-orange-50 shadow-xl'>
             <h1 className='text-5xl m-8 '> Register </h1>
-            <div className='m-8'>
+            <div className='m-8 flex justify-between'>
                 <label className='pr-4'>Name:</label>
-                <input type='password' />
+                <input className='rounded' type='password' />
             </div>
-            <div className='m-8'>
+            <div className='m-8 flex justify-between'>
                 <label className='pr-4'>User Name:</label>
-                <input type='text' /> 
+                <input className='rounded' type='text' /> 
             </div>
-            <div className='m-8'>
+            <div className='m-8 flex justify-between'>
                 <label className='pr-4'>Password:</label>
-                <input type='password' />
+                <input className='rounded' type='password' />
             </div>
         </div>
     );
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,13 +4,13 @@ function SignIN() {
   return (
     <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center'>
             <h1 className='text-5xl m-8 '> Sign In </h1>
-            <div className='m-8'>
+            <div className='m-8 flex justify-between'>
                 <label className='pr-4'>User Name:</label>
-                <input type='text' /> 
+                <input className='rounded' type='text' /> 
             </div>
-            <div className='m-8'>
+            <div className='m-8 flex justify-between'>
                 <label className='pr-4'>Password:</label>
-                <input type='password' />
+                <input className='rounded' type='password' />
             </div>
         </div>
   )
