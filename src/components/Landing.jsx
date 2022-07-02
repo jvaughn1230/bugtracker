1ff1e5522a618905e33478aaa78378fad5bb99f1
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-02 15:52:43 -0700

    adding conditional rendering

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Landing.css';
 
@@ -6,13 +6,19 @@ import SignIn from './SignIn';
 import Register from './Register';
 
 function Landing() {
+
+  const[isRegistered, setIsRegistered] = useState(false);
+  
   return (
     <div className=' grid grid-rows-2 grid-cols-1 w-screen h-screen landing-header'>
         <div className='justify-self-end m-1 mr-2.5'> 
-            <a href='/' className='mr-2.5'>Register</a>
-            <a href='/'>Sign-in</a>
+            <a href='/' onClick={()=>setIsRegistered(false)} className='mr-2.5'>Register</a>
+            <a href='/' onClick={(e)=>{
+                e.preventDefault();
+                setIsRegistered(true)}}>Sign-in</a>
         </div>
-        <Register />
+        {isRegistered ? <SignIn />: <Register />}
+        {console.log(isRegistered)}
     </div>
   );
 }
