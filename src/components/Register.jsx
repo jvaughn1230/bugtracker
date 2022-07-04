f106cdfa92b261487595ffefc5c50fc42ae9bdf3
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-07-04 11:40:35 -0700

    updating signin and register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Register() {   
   return (
-    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center shadow-orange-50 shadow-xl'>
+    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center border-white border-solid border-2 p-4'>
             <h1 className='text-5xl m-8 '> Register </h1>
             <div className='m-8 flex justify-between'>
                 <label className='pr-4'>Name:</label>
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SignIN() {
   return (
-    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center'>
+    <div className='max-w-lg my-0 mx-auto h-80 justify-self-center self-center border-white border-solid border-2 p-4'>
             <h1 className='text-5xl m-8 '> Sign In </h1>
             <div className='m-8 flex justify-between'>
                 <label className='pr-4'>User Name:</label>
